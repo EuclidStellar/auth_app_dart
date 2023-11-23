54a8a3f839de131eb5a52e72dd156d0b55b2d1f1
Gaurav Singh <100860877+EuclidStellar@users.noreply.github.com>
2023-11-23 19:24:09 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,7 @@ import 'firebase_options.dart';
 // FIrebase Implmentation is going to start from this commit 
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
+  
   await Firebase.initializeApp(
   options: DefaultFirebaseOptions.currentPlatform,
 );
